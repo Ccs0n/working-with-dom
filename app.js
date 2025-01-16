f7145c07d3038e942322fab595019f68fe0d84f5
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-01-16 02:04:46 +0200

    skimming the surface of DOM manipulation

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 "use strict";
+
+console.dir(document.getElementById('main-title'));
+console.log(document.querySelector('.list-item'));
+console.log(document.querySelectorAll('.list-item'));
+
+console.log(document.querySelector('ul li:first-of-type'));
+console.log(document.querySelector('ul li:last-of-type'));
