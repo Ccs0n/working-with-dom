5995c6561e1b9c24069c64578b39c6ec92080c73
Menelaos Gkotsopoulos <menelaosgkotsopoulos@gmail.com>
2025-01-17 01:06:28 +0200

    actual DOM manipulation

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 "use strict";
 
-console.dir(document.getElementById('main-title'));
-console.log(document.querySelector('.list-item'));
-console.log(document.querySelectorAll('.list-item'));
+// console.dir(document.getElementById('main-title'));
+// console.log(document.querySelector('.list-item'));
+// console.log(document.querySelectorAll('.list-item'));
+//
+// console.log(document.querySelector('ul li:first-of-type'));
+// console.log(document.querySelector('ul li:last-of-type'));
 
-console.log(document.querySelector('ul li:first-of-type'));
-console.log(document.querySelector('ul li:last-of-type'));
+const h1 = document.getElementById("main-title");
+h1.textContent = "Hello World!";
+h1.style.color = "red";
+h1.style.backgroundColor = "black";
+h1.style.padding = "20px";
+
+const li = document.querySelector("li:last-of-type");
+li.textContent = li.textContent + ' (Changed!)';
+
+// const listItemElements = document.querySelectorAll("li");
+const listItemElements = document.getElementsByTagName("li");
+
+for (const listItemEl of listItemElements) {
+    console.dir(listItemEl);
+}
